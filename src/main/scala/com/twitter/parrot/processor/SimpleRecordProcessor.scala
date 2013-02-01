f07d812a012adf9e285a95865fdc961161911dc8
Roger Chang <roger@sonar.me>
2013-02-01 18:25:09 -0500

    random ip

--- src/main/scala/com/twitter/parrot/processor/SimpleRecordProcessor.scala
+++ src/main/scala/com/twitter/parrot/processor/SimpleRecordProcessor.scala
@@ -22,6 +22,8 @@ import com.twitter.parrot.thrift.ParrotJob
 import com.twitter.parrot.util.UriParser
 import com.twitter.util.{Return, Throw}
 import org.jboss.netty.handler.codec.http.HttpResponse
+import com.google.common.net.InetAddresses
+import java.util.Random
 
 /**
  * This processor just takes a line-separated list of URIs and turns them into requests, for instance:
@@ -39,7 +41,8 @@ class SimpleRecordProcessor(service: ParrotService[ParrotRequest, HttpResponse],
       UriParser(line) match {
         case Return(uri) =>
           if (!uri.path.isEmpty && !line.startsWith("#")) {
-            val body = "{\"id\" : \"BidRequest1\", \"at\" : 1, \"tmax\" : 100,  \"imp\" : [ {\"impid\" : \"BidRequest1Impression1\", \"wseat\" : [ \"seat\" ],    \"h\" : 200,    \"w\" : 300,    \"pos\" : 18,    \"instl\" : 18,    \"btype\" : [ \"btype\" ],    \"battr\" : [ \"battr\" ] } ],  \"site\" : {\"sid\": \"sonar.me\", \"name\": \"sonar.me\", \"domain\": \"sonar.me\", \"pid\": \"pid\", \"pub\": \"pub\", \"pdomain\": \"pdomain\", \"cat\": [ ], \"keywords\": \"foo,bar,keywords\",  \"page\": \"page\", \"ref\":\"ref\", \"search\": \"search\"  }, \"app\" : null,  \"device\" : { \"did\": \"foo\", \"dpid\":\"asdf\", \"country\": \"USA\", \"carrier\":\"carrier\", \"ua\": \"ua\", \"make\":\"make\", \"model\":\"iphone\", \"os\":\"ios\", \"osv\":\"5\", \"js\":0, \"loc\": \"40.750580,-73.993580\", \"ip\":\"69.38.227.134\"},  \"user\" : {\"uid\":\"bar\", \"yob\":4, \"gender\":\"male\", \"zip\":\"10003\", \"country\":\"USA\", \"keywords\":\"keyword\"},  \"restrictions\": null }"
+              val ip = InetAddresses.fromInteger(new Random().nextInt()).getHostAddress
+            val body = "{\"id\" : \"BidRequest1\", \"at\" : 1, \"tmax\" : 100,  \"imp\" : [ {\"impid\" : \"BidRequest1Impression1\", \"wseat\" : [ \"seat\" ],    \"h\" : 200,    \"w\" : 300,    \"pos\" : 18,    \"instl\" : 18,    \"btype\" : [ \"btype\" ],    \"battr\" : [ \"battr\" ] } ],  \"site\" : {\"sid\": \"sonar.me\", \"name\": \"sonar.me\", \"domain\": \"sonar.me\", \"pid\": \"pid\", \"pub\": \"pub\", \"pdomain\": \"pdomain\", \"cat\": [ ], \"keywords\": \"foo,bar,keywords\",  \"page\": \"page\", \"ref\":\"ref\", \"search\": \"search\"  }, \"app\" : null,  \"device\" : { \"did\": \"foo\", \"dpid\":\"asdf\", \"country\": \"USA\", \"carrier\":\"carrier\", \"ua\": \"ua\", \"make\":\"make\", \"model\":\"iphone\", \"os\":\"ios\", \"osv\":\"5\", \"js\":0, \"loc\": \"40.750580,-73.993580\", \"ip\":\"" + ip + "\"},  \"user\" : {\"uid\":\"bar\", \"yob\":4, \"gender\":\"male\", \"zip\":\"10003\", \"country\":\"USA\", \"keywords\":\"keyword\"},  \"restrictions\": null }"
             val request = new ParrotRequest(target, None, Nil, uri, line, method = "POST", body = body)
             Some(service(request))
           }
